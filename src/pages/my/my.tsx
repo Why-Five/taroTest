bcfd9988a3f088bd635e4e5038313770a06eb5bb
Why-02 <why1605044412@163.com>
2024-10-29 16:04:30 +0800

    用户登录入口

--- src/pages/my/my.tsx
+++ src/pages/my/my.tsx
@@ -1,9 +1,22 @@
-import { View, Text } from '@tarojs/components';
+import { View } from '@tarojs/components';
+import Taro from '@tarojs/taro';
+import { useAppSelector } from 'src/store';
+import { AtButton, AtList, AtListItem } from 'taro-ui';
 
 export default function My() {
+  // 获取Redux中用户数据
+  const user = useAppSelector((state) => state.user.userInfo);
+
+  const handleClick = () => {
+    Taro.navigateTo({ url: '/pages/login/login' });
+  };
   return (
     <View>
-      <Text>My</Text>
+      <View>用户信息:{JSON.stringify(user)}</View>
+      <AtButton type="primary">清理用户信息</AtButton>
+      <AtList>
+        <AtListItem title="去登录" onClick={handleClick} arrow="right" />
+      </AtList>
     </View>
   );
 }
