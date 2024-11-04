2923af4369e91ad99bb275655440d057e06cbdf5
Why-02 <why1605044412@163.com>
2024-11-04 16:14:22 +0800

    获取公告分页列表数据

--- src/pages/notice/notice.tsx
+++ src/pages/notice/notice.tsx
@@ -1,33 +1,92 @@
-import { View, Text, RichText , Navigator } from '@tarojs/components';
-import { useState } from 'react';
+import { View, Text, RichText , Navigator, ScrollView } from '@tarojs/components';
+import { useEffect, useState } from 'react';
 import './notice.scss';
+import { getNoticePage } from '@/service/notice';
+import Taro from '@tarojs/taro';
 
 const Notice = () => {
+  // const [noticeList, setNoticeList] = useState<IndexNotice>({
+  //   list: [
+  //     {
+  //       pkId: 1,
+  //       createTime: '2024-01-01',
+  //       title: '资源分享应用维护',
+  //       content: '<p>资源分享应用维护</p>',
+  //       adminId: 1,
+  //       isTop: 0,
+  //       adminName: 'admin',
+  //     },
+  //     {
+  //       pkId: 2,
+  //       createTime: '2024-01-01',
+  //       title: '资源分享应用关闭',
+  //       content: '<p>资源分享应用关闭</p>',
+  //       adminId: 1,
+  //       isTop: 0,
+  //       adminName: 'admin',
+  //     }
+  //   ],
+  //   total: 0,
+  // });
   const [noticeList, setNoticeList] = useState<IndexNotice>({
-    list: [
-      {
-        pkId: 1,
-        createTime: '2024-01-01',
-        title: '资源分享应用维护',
-        content: '<p>资源分享应用维护</p>',
-        adminId: 1,
-        isTop: 0,
-        adminName: 'admin',
-      },
-      {
-        pkId: 2,
-        createTime: '2024-01-01',
-        title: '资源分享应用关闭',
-        content: '<p>资源分享应用关闭</p>',
-        adminId: 1,
-        isTop: 0,
-        adminName: 'admin',
-      }
-    ],
-    total: 0,
+    list: [],
+    total: 0
   });
-return (
-  <View className='noticePage'>
+  const [finish, setFinish] = useState(false);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+
+  const getAllNotice = async () => {
+    if (!finish) {
+      try {
+        const res = await getNoticePage({
+          page,
+          limit
+        });
+
+        setNoticeList(prev => {
+          const newList = [...prev.list, ...res.data.list];
+          const newTotal = res.data.total;
+
+          console.log('查询条件：', page, limit);
+
+          const isFinished = newList.length >= newTotal;
+          if (isFinished) {
+            setFinish(true);
+          }
+
+          if (newList.length <= newTotal) {
+            setPage(pre => pre + 1);
+          }
+
+          return {
+            list: newList,
+            total: newTotal
+          };
+        });
+      } catch (error) {
+        console.error('获取公告列表失败', error);
+        Taro.showToast({ title: '加载失败，请重试', icon: 'none' });
+      }
+    } else {
+      Taro.showToast({ title: '没有更多数据', icon: 'none' });
+    }
+  };
+
+  useEffect(() => {
+    getAllNotice();
+  }, []);
+
+  const handleReachBottom = () => {
+    console.log('触底了');
+    getAllNotice();
+  };
+
+  return (
+  <ScrollView scrollY
+    onScrollToLower={handleReachBottom}
+    style={{height: '100vh'}} className='noticePage'
+  >
     {noticeList.list.map((item) => (
       <View className='item-card' key={item.pkId}>
         <View className='time'>{item.createTime}</View>
@@ -47,8 +106,10 @@ return (
         </View>
       </View>
 ))}
-    <View className='loading-text'>没有更多数据</View>
-  </View>
+      <View className='loading-text'>
+        {finish ? '没有更多数据' : '加载中...'}
+      </View>
+  </ScrollView>
 );
 };
 
