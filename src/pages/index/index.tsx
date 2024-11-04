4473c6c3352b97bf3e7180fd51ce80b162563e2d
Why-02 <why1605044412@163.com>
2024-11-04 14:55:49 +0800

    获取首页公告数据

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,14 +1,31 @@
-import { View, Text } from '@tarojs/components';
-import { AtButton } from 'taro-ui';
+import { View, Text, SwiperItem } from '@tarojs/components';
+import { useEffect, useState } from 'react';
+import { getIndexNotice } from '@/service/notice';
 import 'taro-ui/dist/style/index.scss';
 import 'taro-ui/dist/style/components/button.scss'; // 按需引入
 import './index.scss';
 import Navbar from './components/CustomNavbar';
+import Notice from './components/Notice';
+
 
 export default function Index() {
+  const [noticeList, setNoticeList] = useState<SwiperItem[]>([]);
+  const [swiper, setSwiper] = useState<SwiperItem[]>([]);
+
+  const getIndexNoticeFunction = async () => {
+    const res = await getIndexNotice();
+    if (res.code === 0) {
+      setNoticeList(res.data);
+    }
+  };
+
+  useEffect(() => {
+    getIndexNoticeFunction();
+  },[])
   return (
     <View className='index'>
       <Navbar />
+      <Notice noticeList={noticeList} />
     </View>
   );
 }
