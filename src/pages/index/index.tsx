b0d1948a8ecd4b05ef81370c0e0834791723e6e9
Why-02 <why1605044412@163.com>
2024-11-04 14:23:41 +0800

    头部导航栏配置

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,16 +3,12 @@ import { AtButton } from 'taro-ui';
 import 'taro-ui/dist/style/index.scss';
 import 'taro-ui/dist/style/components/button.scss'; // 按需引入
 import './index.scss';
+import Navbar from './components/CustomNavbar';
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello world!</Text>
-      <AtButton type="primary">I need Taro UI</AtButton>
-      <Text>Taro UI 支持 Vue 了吗？</Text>
-      <AtButton type="primary" circle>支持</AtButton>
-      <Text>共建？</Text>
-      <AtButton type="secondary" circle>来</AtButton>
+    <View className='index'>
+      <Navbar />
     </View>
   );
 }
