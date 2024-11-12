834697ce0330cd77e8dd8f96e9ee5965315d17e4
Why-02 <why1605044412@163.com>
2024-11-12 17:18:15 +0800

    历史搜索记录

--- src/pages/search/search.tsx
+++ src/pages/search/search.tsx
@@ -1,8 +1,10 @@
+import Taro from "@tarojs/taro";
 import { View } from "@tarojs/components";
-import { AtSearchBar } from "taro-ui";
+import { AtIcon, AtSearchBar } from "taro-ui";
 import { useEffect, useState } from "react";
 import { getTagsList } from "@/service/contribute";
 import './search.scss'
+
 
 
 
@@ -43,7 +45,23 @@ export default function Search() {
     setQueryParams({ ...queryParams, tagId: item.pkId })
   };
 
+  const [historySearch,setHistorySearch] = useState<string[]>(Taro.getStorageSync('historySearch')||['视频','项目']);
 
+  const removeHistory = () => {
+    Taro.showModal({
+      title: '确定清除历史搜索记录？',
+      success: (res) => {
+        if (res.confirm) {
+          Taro.removeStorageSync('historySearch');
+          setHistorySearch([]);
+        }
+      },
+    });
+  };
+
+  const clickHistoryTab = (tab: string) => {
+    setQueryParams({ ...queryParams, keyword: tab });
+  };
   return (
     <View className='searchLayout'>
       <View className='search>'>
@@ -77,6 +95,29 @@ export default function Search() {
             </View>
           </>
         )}
+      </View>
+      <View className='history'>
+              {historySearch.length > 0 && (
+                <>
+                  <View className='topTitle'>
+                    <View className='text'>最近搜索</View>
+                    <View className='icon' onClick={removeHistory}>
+                      <AtIcon value='trash' size='25' />
+              </View>
+              </View>
+                    <View className='tabs'>
+                      {historySearch.map((tab) => (
+                        <View
+                          key={tab}
+                          className='tab'
+                          onClick={() => clickHistoryTab(tab)}
+                        >
+                        {tab}
+                        </View>
+                      ))}
+                    </View>
+            </>
+          )}
       </View>
     </View>
   );
