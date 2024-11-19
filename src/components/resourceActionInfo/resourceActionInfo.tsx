94deb283ad962a6e34c0aee0f65896c3480dc19a
Why-02 <why1605044412@163.com>
2024-11-19 17:49:55 +0800

    resourceActionInfo

---
+++ src/components/resourceActionInfo/resourceActionInfo.tsx
@@ -0,0 +1,31 @@
+import { View , Text} from "@tarojs/components";
+import { AtIcon } from "taro-ui";
+
+
+interface InfoProps {
+  info: {
+    title: string;
+    createTime: string;
+    price: number;
+  };
+}
+
+const ResourceItem = ({ info }: InfoProps)=> {
+  return (
+    <View className='list'>
+      <View className='left'>
+        <AtIcon className='icons' value='medal' size='30' color='#f67f67' />
+        <View className='info'>
+          <Text className='title'>{info.title}</Text>
+          <Text className='tips'>{info.createTime}</Text>
+        </View>
+      </View>
+      <View className='right'>
+        <View className='bonus'>{info.price}积分</View>
+        <View className='txt'>兑换成功</View>
+      </View>
+    </View>
+  )
+}
+
+export default ResourceItem;
---
+++ src/pages/bonus/bonus.tsx
@@ -0,0 +1,9 @@
+import { View , Text} from "@tarojs/components";
+
+export default function Bonus() {
+  return (
+    <View className='page'>
+      <Text>积分明细</Text>
+    </View>
+  )
+}
